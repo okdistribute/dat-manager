61ca23ff82bd5be6f948121ec7475a828beeaeb7
Karissa McKelvey <krmckelv@gmail.com>
2015-09-21 13:38:52 -0700

    modularize routes

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var http = require('http')
-var url = require('url')
-var formatData = require('format-data')
-var jsonBody = require('body/json')
 var Router = require('http-hash-router')
-var pump = require('pump')
 var level = require('level')
 var subdown = require('subleveldown')
-var ndjson = require('ndjson')
-
-var initDat = require('./lib/init-dat.js')
-var removeDat = require('./lib/remove-dat.js')
+var routes = require('./lib/routes.js')
 
 module.exports = function manager (opts) {
   if (!opts) return manager({})
@@ -44,57 +37,16 @@ function createRouter () {
   var router = Router()
 
   router.set('/', function (req, res, opts, cb) {
-    if (req.method === 'GET') return all(req, res, opts, cb)
+    if (req.method === 'GET') return routes.all(req, res, opts, cb)
     else res.end('Method not allowed.')
   })
 
   router.set('/:name', function (req, res, opts, cb) {
-    if (req.method === 'PUT')  return get(req, res, opts, cb)
-    else if (req.method === 'POST')  return create(req, res, opts, cb)
-    else if (req.method === 'DELETE') return remove(req, res, opts, cb)
+    if (req.method === 'PUT')  return routes.get(req, res, opts, cb)
+    else if (req.method === 'POST')  return routes.create(req, res, opts, cb)
+    else if (req.method === 'DELETE') return routes.remove(req, res, opts, cb)
     return res.end('Method not allowed.')
   })
 
   return router
-}
-
-function get (req, res, opts, cb) {
-  opts.db.dats.get(opts.params.name, function (err, dat) {
-    if (err) return cb(err)
-    res.setHeader("content-type", "application/json")
-    res.end(JSON.stringify(dat))
-  })
-}
-
-function all (req, res, opts, cb) {
-  var parsed = url.parse(req.url, true)
-  var query = parsed.query
-  if (!query.format) query.format = 'ndjson'
-  pump(opts.db.dats.createValueStream(), formatData(query.format), res, function (err) {
-    if (err) return cb(err)
-  })
-}
-
-function create (req, res, opts, cb) {
-  jsonBody(req, res, function (err, body) {
-    if (err) return cb(err)
-    var data = {
-      user: body.user,
-      hostname: opts.hostname,
-      name: opts.params.name
-    }
-    initDat(opts.db.dats, data, function (err, dat) {
-      if (err) return cb(err)
-      res.setHeader("content-type", "application/json")
-      res.end(JSON.stringify(dat))
-    })
-  })
-}
-
-function remove (req, res, opts, cb) {
-  removeDat(opts.db.dats, {name: opts.params.name}, function (err) {
-    if (err) return cb(err)
-    res.setHeader("content-type", "application/json")
-    res.end(JSON.stringify({deleted: true}))
-  })
 }

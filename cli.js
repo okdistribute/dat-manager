b02486e8923de2679307e612a0956ad154f04941
Karissa McKelvey <krmckelv@gmail.com>
2015-09-20 23:43:48 -0700

    add delete/get all

--- cli.js
+++ cli.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 var args = require('minimist')(process.argv.slice(2))
-var manager = require('./')
+var path = require('path')
 var getport = require('getport')
+var manager = require('./')
 
-if (args.port) return manager(args.port)
+var configPath = args.config || path.join(__dirname, 'config.json')
+var config = JSON.parse(fs.readFileSync(configPath).toString())
+
+for (var v in config) {
+  if (process.env.hasOwnProperty(v)) {
+    config[v] = process.env[v]
+  }
+}
+
+if (config.port) return manager(config)
 
 getport(5000, function (err, port) {
   if (err) throw err
-  var server = manager()
+  var server = manager(config)
   server.listen(port, function (err) {
     if (err) throw err
     console.log('Listening on port', port)
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var http = require('http')
 var jsonBody = require('body/json')
 var Router = require('http-hash-router')
+var pump = require('pump')
 var level = require('level')
 var subdown = require('subleveldown')
 var ndjson = require('ndjson')
-var createDat = require('./create-dat.js')
+
+var initDat = require('./lib/init-dat.js')
+var removeDat = require('./lib/remove-dat.js')
 
 module.exports = function manager (opts) {
   if (!opts) return manager({})
@@ -38,18 +41,53 @@ function createRouter () {
   var router = Router()
 
   router.set('/', function (req, res, opts, cb) {
-    if (req.method === 'GET') return opts.db.dats.createReadStream().pipe(ndjson.serialize()).pipe(res)
-    else if (req.method !== 'POST') return res.end('Method not allowed.')
+    if (req.method === 'GET') return all(req, res, opts, cb)
+    else res.end('Method not allowed.')
+  })
 
-    jsonBody(req, res, function (err, body) {
-      if (err) return cb(err)
-      createDat(opts.db.dats, {user: body.user, name: body.name}, function (err, dat) {
-        if (err) return cb(err)
-        res.setHeader("content-type", "application/json")
-        return res.end(JSON.stringify(body))
-      })
-    })
+  router.set('/:name', function (req, res, opts, cb) {
+    if (req.method === 'PUT')  return get(req, res, opts, cb)
+    else if (req.method === 'PUT')  return update(req, res, opts, cb)
+    else if (req.method === 'POST')  return create(req, res, opts, cb)
+    else if (req.method === 'DELETE') return remove(req, res, opts, cb)
+    return res.end('Method not allowed.')
   })
 
   return router
+}
+
+function get (req, res, opts, cb) {
+  opts.db.dats.get(opts.params.name, function (err, dat) {
+    if (err) return cb(err)
+    res.setHeader("content-type", "application/json")
+    res.end(JSON.stringify(dat))
+  })
+}
+
+function all (req, res, opts, cb) {
+  pump(opts.db.dats.createReadStream(), ndjson.serialize(), res, function (err) {
+    if (err) return cb(err)
+  }
+}
+
+function create (req, res, opts, cb) {
+  jsonBody(req, res, function (err, body) {
+    if (err) return cb(err)
+    initDat(opts.db.dats, {user: body.user, name: body.name}, function (err, dat) {
+      if (err) return cb(err)
+      res.setHeader("content-type", "application/json")
+      return res.end(JSON.stringify(dat))
+    })
+  })
+}
+
+function remove (req, res, opts, cb) {
+  jsonBody(req, res, function (err, body) {
+    if (err) return cb(err)
+    removeDat(opts.db.dats, opts.params.name, function (err) {
+      if (err) return cb(err)
+      res.setHeader("content-type", "application/json")
+      return res.end(JSON.stringify({deleted: true}))
+    })
+  })
 }

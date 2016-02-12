27a7f65db590432b18cb3605b0d6c46900f54e8e
Karissa McKelvey <krmckelv@gmail.com>
2016-02-11 19:16:08 -0800

    expand module, with basic functions and tests

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var mkdirp = require('mkdirp')
 var level = require('level')
+var parallel = require('run-parallel')
 var debug = require('debug')('dat-manager')
 var path = require('path')
 var Dat = require('dat')
-var subdown = require('subleveldown')
 
 module.exports = Manager
 
@@ -11,23 +11,45 @@ function Manager (opts) {
   if (!(this instanceof Manager)) return new Manager(opts)
   if (!opts) opts = {}
   this.db = opts.db || createDb(opts)
-  this.DOWNLOAD_PATH = path.join(process.cwd(), 'dats')
+  this.location = opts.location || path.join(process.cwd(), 'dats')
   this.running = {}
 }
 
 Manager.prototype.get = function (name, cb) {
+  if (!name) return cb(new Error('Name required'))
+  return this.db.get(name, cb)
+}
+
+Manager.prototype.stop = function (name, cb) {
+  var self = this
+  if (!name) return cb(new Error('Name required'))
+  var dat = this.running[name]
+  if (!dat) return cb(new Error('No dat running with that name'))
+  if (dat.close) dat.close(done)
+  else done()
+
+  function done (err) {
+    if (err) return cb(err)
+    dat.state = 'inactive'
+    self.running[name] = undefined
+    self.db.put(name, dat, function (err) {
+      if (err) return cb(err)
+      cb(null, dat)
+    })
+  }
 }
 
 Manager.prototype.start = function (name, link, cb) {
   var self = this
   if (!name) return cb(new Error('Name required'))
+  if (!link) return cb(new Error('Link required'))
   if (self.running[name]) return cb(new Error('Name taken'))
   var db = Dat()
   debug('downloading', link)
   var dat = {
     name: name,
     link: link,
-    path: path.join(self.DOWNLOAD_PATH, link)
+    path: path.join(self.location, link)
   }
   db.download(link, dat.path, done)
 
@@ -38,49 +60,44 @@ Manager.prototype.start = function (name, link, cb) {
     dat.state = 'active'
     dat.date = Date.now()
     self.running[name] = dat
-    if (cb) cb(null, dat)
+    self.db.put(name, dat, function (err) {
+      if (err) return cb(err)
+      return cb(null, dat)
+    })
   }
 }
 
-Manager.prototype.list = function (name, cb) {
+Manager.prototype.createValueStream = function () {
+  return this.db.createValueStream()
 }
 
 Manager.prototype.delete = function (name, cb) {
-}
-
-// STUBS BELOW:
-
-function restart (dat, cb) {
-  debug('restarting', dat)
-  stop(dat, function (err, dat) {
-    debug('done', arguments)
-    if (err) throw err
-    start(dat, cb)
+  var self = this
+  self.db.del(name, function (err) {
+    if (err) return cb(err)
+    self.running[name] = undefined
   })
 }
 
-
-function stop (dat, cb) {
-  config.read()
-  var close = RUNNING[dat.path]
-  debug('stopping', dat)
-  if (close) close(done)
-  else done()
-
-  function done (err) {
-    debug('done', err)
-    if (err) return cb(err)
-    RUNNING[dat.path] = undefined
-    dat.state = 'inactive'
-    config.update(dat)
-    if (cb) cb(null, dat)
-  }
+Manager.prototype.close = function (cb) {
+  var self = this
+  var stream = self.db.createKeyStream()
+  var funcs = []
+  stream.on('data', function (name) {
+    funcs.push(function (done) {
+      self.stop(name, done)
+    })
+  })
+  stream.on('end', function () {
+    parallel(funcs, function (err) {
+      if (err) return cb(err)
+      return cb()
+    })
+  })
 }
 
 function createDb (opts) {
   opts.DB_PATH = opts.DB_PATH || './data'
   mkdirp.sync(opts.DB_PATH)
-  var db = level(opts.DB_PATH)
-  db.dats = subdown(db, 'dats', {valueEncoding: 'json'})
-  return db
+  return level(opts.DB_PATH)
 }
---
+++ tests/test.js
@@ -0,0 +1,29 @@
+var Manager = require('../')
+var Dat = require('dat')
+var rimraf = require('rimraf')
+var path = require('path')
+var memdb = require('memdb')
+var test = require('tape')
+
+var location = path.join(__dirname, 'manager_test')
+
+test('manager start and stop', function (t) {
+  var manager = Manager({
+    location: location,
+    db: memdb()
+  })
+  var db = Dat()
+  db.addFiles(__dirname, function (err, link) {
+    t.ifError(err)
+    manager.start('mydat', link, function (err) {
+      t.ifError(err)
+      manager.stop('mydat', function (err) {
+        t.ifError(err)
+        //rimraf.sync(location)
+        manager.close(function () {
+          t.end()
+        })
+      })
+    })
+  })
+})

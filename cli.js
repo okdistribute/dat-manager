f71f8f006c880957548017c9f950f9eb9739a8cb
Karissa McKelvey <krmckelv@gmail.com>
2016-02-29 18:05:56 -0800

    cleanup unused cli

--- cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var args = require('minimist')(process.argv.slice(2))
-var server = require('./app/server.js')
-
-var port = 8080 || process.env.PORT
-
-server.listen(port, function () {
-  console.log('http://localhost:' + port)
-})
